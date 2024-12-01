e20c71cf5fb14f08ddac8cb1499740fefe80967b
adodoCoder <alavropulido@whccd.edu>
2024-11-30 18:33:06 -0800

    added final answers and comments

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
-console.log("Over the last 9 weeks the modules and lecture activities were always a big help with some of the assignments.");
+console.log("Over the last 9 weeks the modules and lecture activities were always a big help with some of the assignments."); // Answer 1 */
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("For this course specifically I would like for it to be a longer than 9 weeks. Learning JavaScript at a fast pace does not help with retaining information.");
+console.log("For this course specifically I would like for it to be a longer than 9 weeks. Learning JavaScript at a fast pace does not help with retaining information."); // Answer 2 */
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
-console.log("");
+console.log("I feel like some of the concepts did stick and others just flew over my head. The terminology for this course was harder to learn than that last two."); // Answer 3 */
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
@@ -24,4 +24,4 @@ console.log("");
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
-console.log("");
+console.log("Everything was new to me since I have no experience in coding at. HTML and CSS are easier to understand because both go together."); // Answer 4 */
